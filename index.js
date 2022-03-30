305b81f2308bd8a7acb2dbb0ff296f42e86c7ff8
Philip D <philip.devries@system1.com>
2022-03-30 11:43:37 -0400

    sample question for filesystem writing/reading in Node

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const exampleTermsFile = './exampleTerms.json';
+
+const financeTerms = {
+  "Finance" : [
+    "Mortgage",
+    "Investing",
+    "Debt"
+  ]
+}
+
+// your solution below
+
